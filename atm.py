bf6d9db8f249595cf21864f7b8405ed9148e33da
Alon Blyachman <alon.blyachman@gmail.com>
2024-11-10 16:00:01 +0200

    Added ATM

--- atm.py
+++ atm.py
@@ -19,10 +19,62 @@ class AccountManager(ABC):
 
 
     @abstractmethod
-    def deposit_money(self, account_number):
+    def deposit_money(self, account_number, sum):
         pass
 
 
     @abstractmethod
-    def withdraw_money(self, account_number):
+    def withdraw_money(self, account_number, sum):
         pass
+
+
+    @abstractmethod
+    def change_password(self, account_number, new_password):
+        pass
+    
+
+class ATM(ABC):
+    def __init__(self, account_manager:AccountManager):
+        self.account_manager = account_manager
+
+
+    @abstractmethod
+    def run_atm(self):
+        pass
+
+
+    @abstractmethod
+    def stop_atm(self):
+        pass
+
+    
+    @abstractmethod
+    def connect_to_atm(self, id_number):
+        pass
+
+    
+    @abstractmethod
+    def connect_to_account(self, account_number, password):
+        pass
+
+
+    @abstractmethod
+    def check_balance(self, account_number):
+        pass
+
+
+    def withdraw_money(self, account_number, sum):
+        return self.account_manager.withdraw_money(account_number, sum)
+    
+
+    def deposit_money(self, account_number, sum):
+        return self.account_manager.deposit_money(account_number, sum)
+    
+
+    def change_password(self, account_number, new_password):
+        return self.account_manager.change_password(account_number, new_password)
+    
+
+    def transfer_money(self, account_withdraw, account_deposit, sum):
+        self.withdraw_money(account_withdraw, sum)
+        self.deposit_money(account_deposit, sum)
